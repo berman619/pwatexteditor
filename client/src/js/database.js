44578ba8c1aa0066ffac6b1310fd4fe88120aa91
Zach Berger <zachhberger@gmail.com>
2023-06-23 15:34:53 -0400

    wrote functions for data persistence

---
+++ client/src/js/database.js
@@ -0,0 +1,46 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  try {
+    console.log('Post to the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ jate: content });
+    const result = await request;
+    console.log ('Data saved to the database', result);
+  } catch(err) {
+    console.error('An error occurred:', err);
+  }
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  try {
+    console.log ('Get all from the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log ('result.value', result);
+    return result;
+  } catch(err) {
+    console.error('An error occurred:', err);
+  }
+};
+
+initdb();
